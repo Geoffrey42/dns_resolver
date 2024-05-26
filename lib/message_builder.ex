03a30854dd2704df8e1a6f81ce7c381104f04af8
Geoffrey Gane <geoffreygane@gmail.com>
2024-05-26 22:43:00 +0200

    feat(MessageBuilder): build question_section

--- lib/message_builder.ex
+++ lib/message_builder.ex
@@ -1,12 +1,23 @@
 defmodule MessageBuilder do
   @spec build(binary()) :: bitstring()
-  def build(_hostname) do
-    header()
+  def build(hostname) do
+    header() <> question_section(hostname)
   end
 
   defp header() do
     id = :rand.uniform(65536)
 
     <<id::16, 1::1, 0::4, 0::1, 0::1, 1::1, 0::1, 0::3, 0::4, 1::16, 0::16, 0::16, 0::16>>
+  end
+
+  defp question_section(hostname) do
+    qname =
+      String.split(hostname, ".")
+      |> Enum.map(fn label -> <<String.length(label)>> <> label end)
+      |> Enum.join()
+      |> Kernel.<>(<<0>>)
+    
+
+    qname <> <<1::16>> <> <<1::16>>
   end
 end
--- test/message_builder_test.exs
+++ test/message_builder_test.exs
@@ -1,34 +1,51 @@
 defmodule MessageBuilderTest do
   use ExUnit.Case
 
-  test "should build a valid message given a hostname" do
-    given_hostname = "www.google.com"
+  describe "should build a valid message given a hostname" do
+    test "notably a header" do
+      given_hostname = "www.google.com"
 
-    message = MessageBuilder.build(given_hostname)
+      message = MessageBuilder.build(given_hostname)
 
-    assert << header::binary-size(12), _rest::binary >> = message
-    assert <<
-             _id::16,
-             qr::1,
-             opcode::4,
-             _aa::1,
-             _tc::1,
-             rd::1,
-             _ra::1,
-             _z::3,
-             _rcode::4,
-             qdcount::16,
-             ancount::16,
-            nscount::16,
-            arcount::16
-           >> = header
+      assert <<header::binary-size(12), _rest::binary>> = message
 
-    assert qr == 1
-    assert opcode == 0
-    assert rd == 1
-    assert qdcount == 1
-    assert ancount == 0
-    assert nscount == 0
-    assert arcount == 0
+      assert <<
+               _id::16,
+               qr::1,
+               opcode::4,
+               _aa::1,
+               _tc::1,
+               rd::1,
+               _ra::1,
+               _z::3,
+               _rcode::4,
+               qdcount::16,
+               ancount::16,
+               nscount::16,
+               arcount::16
+             >> = header
+
+      assert qr == 1
+      assert opcode == 0
+      assert rd == 1
+      assert qdcount == 1
+      assert ancount == 0
+      assert nscount == 0
+      assert arcount == 0
+    end
+
+    test "notably the question section" do
+      given_hostname = "www.google.com"
+
+      message = MessageBuilder.build(given_hostname)
+
+      assert <<_header::binary-size(12), qname::binary-size(16), qtype::16, qclass::16, _rest::binary>> = message
+
+
+      assert qname == << 3, "www", 6, "google", 3, "com", 0 >>
+      assert qtype == 1
+      assert qclass == 1
+    end
+
   end
 end
